2bc545048a2cd55db7d10b6e0b48677e2c51f072
JosephMyers33 <josephmyerssocial@gmail.com>
2025-12-31 14:08:16 -0600

    Change

--- sql-part-2/studio/Studio.sql
+++ sql-part-2/studio/Studio.sql
@@ -1,3 +1,23 @@
-SELECT count(language_code) AS 'ENGLISH TITLES'
-FROM BooksDB.dbo.books
-WHERE LEFT(language_code,2) <> 'en';
+SELECT 
+    ROUND(AVG(A.value), 2) AS AvgEarnings, 
+    2016 AS Year,
+    'Annual' AS Period
+FROM 
+    LaborStatisticsDB.dbo.annual_2016 AS A
+JOIN 
+    LaborStatisticsDB.dbo.series AS S ON A.series_id = S.series_id
+WHERE 
+    S.data_type_code = 30
+
+UNION
+
+SELECT 
+    ROUND(AVG(J.value), 2) AS AvgEarnings, 
+    2017 AS Year,
+    'January' AS Period
+FROM 
+    LaborStatisticsDB.dbo.january_2017 AS J
+JOIN 
+    LaborStatisticsDB.dbo.series AS S ON J.series_id = S.series_id
+WHERE 
+    S.data_type_code = 30;
